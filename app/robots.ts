ca860472b6f3327b790ac06bda9da2516996297d
modrenonline831 <modrenonline831@gmail.com>
2026-02-05 18:34:18 +0200

    seo again3

--- app/robots.ts
+++ app/robots.ts
@@ -1,16 +1,13 @@
 export const dynamic = 'force-static'
 
-mport { MetadataRoute } from 'next'
+import { MetadataRoute } from 'next'
 
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
       userAgent: '*',
       allow: '/',
-      // نمنع الصفحات الخاصة
-      disallow: ['/admin/', '/api/', '/dashboard/', '/private/'],
     },
     sitemap: 'https://modrenonline.com/sitemap.xml',
-    host: 'https://modrenonline.com',
   }
 }
