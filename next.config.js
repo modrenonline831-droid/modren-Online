9eaacaa57e1351460fca7c85b51b948989e69acf
modrenonline831 <modrenonline831@gmail.com>
2026-02-24 09:05:47 +0200

    fix: correct next.config.js syntax

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-<<<<<<< HEAD
     remotePatterns: [
       { protocol: 'http', hostname: 'localhost' },
       { protocol: 'https', hostname: 'vercel.app' },
@@ -10,13 +9,4 @@ const nextConfig = {
   },
   reactStrictMode: true,
 }
-=======
-    unoptimized: true,
-    domains: ['localhost', 'vercel.app'],
-  },
-  reactStrictMode: true,
-  output: 'standalone', // بدل export
-}
-
->>>>>>> 199d54cc9971ba86d5d4515cc073472c7631b9e2
 module.exports = nextConfig
